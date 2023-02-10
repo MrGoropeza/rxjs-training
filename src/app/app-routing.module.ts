bb8fdb486ac50accfb459c21e1e2fc6cb660ab04
MrGoropeza <goropeza8@gmail.com>
2023-02-10 19:56:00 -0300

    feat: initial structure with home & observables features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./home/feature/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'observables',
+    loadChildren: () =>
+      import(
+        './observables/feature/observables-shell/observables-shell.module'
+      ).then((m) => m.ObservablesShellModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/observables/feature/observables-shell/observables-shell.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ObservablesShellRoutingModule } from './observables-shell-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ObservablesShellRoutingModule
+  ]
+})
+export class ObservablesShellModule { }
