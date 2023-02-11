5c77d747d872e8bf03372a20e4e27a6d263509ee
MrGoropeza <goropeza8@gmail.com>
2023-02-11 01:43:32 -0300

    feat: added rxjs logo on sidenav

--- src/app/shared/ui/sidenav/sidenav.module.ts
+++ src/app/shared/ui/sidenav/sidenav.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { SidenavComponent } from './sidenav.component';
 
 import { MatTreeModule } from '@angular/material/tree';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [SidenavComponent],
@@ -16,6 +17,7 @@ import { RouterModule } from '@angular/router';
     MatTreeModule,
     MatButtonModule,
     MatIconModule,
+    MatDividerModule,
   ],
 })
 export class SidenavModule {}
